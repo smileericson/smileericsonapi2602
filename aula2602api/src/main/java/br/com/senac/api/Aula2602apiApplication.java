1c033c13b1d7b3d9399dcb3f539fa2080b578ab5
ERICSON SMILE SILVEIRA <ericson.silveira@alunos.sc.senac.br>
2026-02-26 19:58:36 -0300

    Att Main

--- aula2602api/src/main/java/br/com/senac/api/Aula2602apiApplication.java
+++ aula2602api/src/main/java/br/com/senac/api/Aula2602apiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Aula2602apiApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Aula2602apiApplication.class, args);
 	}
