e6b80a684af8c4320c72ed52a349da94b8fc1dac
ERICSON SMILE SILVEIRA <ericson.silveira@alunos.sc.senac.br>
2026-02-26 20:26:28 -0300

    Parametro

--- aula2602api/src/main/java/br/com/senac/api/controller/HelloWordController.java
+++ aula2602api/src/main/java/br/com/senac/api/controller/HelloWordController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/helloworld")
@@ -13,6 +14,14 @@ public class HelloWordController {
 
     public ResponseEntity<String> teste() {
         return ResponseEntity.ok("Ola mundo!");
+    }
+
+    public ResponseEntity<String> parImpar(
+            @RequestParam int numero){
+        if ((numero%2)==0){
+            return ResponseEntity.ok("Par");
+        }
+        return ResponseEntity.ok("Impar");
     }
 
 
